0c71649905744b3f2b9b8236e158ead4f76f4b2b
EmeraldZhu <zhuemerald@gmail.com>
2023-10-08 16:47:32 -0700

    hot fix - betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
 
+/* This program prints a message to the console */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle\n";
+
 	puts(str);
+
 	return (0);
 }
