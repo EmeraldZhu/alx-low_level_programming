6e965c907f43861d56b1783c81237ee74f19093c
EmeraldZhu <zhuemerald@gmail.com>
2023-10-09 03:00:23 -0700

    function documentation - betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-/* This program prints a message to the console */
+/**
+ * main - Entry point of the program
+ */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle\n";
