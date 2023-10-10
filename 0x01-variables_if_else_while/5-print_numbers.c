6d22509b15893d99dc8f78205644525ff9e6c0ac
EmeraldZhu <zhuemerald@gmail.com>
2023-10-09 18:25:58 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Return: Returns 0
+ */
+int main(void)
+{
+	int single_digit;
+
+	for (single_digit = 0; single_digit <= 9; single_digit++)
+	{
+		printf("%d", single_digit);
+	}
+
+	printf("\n");
+
+	return (0);
+}
