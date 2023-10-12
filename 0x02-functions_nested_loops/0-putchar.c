816952979a37ecedb18933ce31ff1239b8fb2489
EmeraldZhu <zhuemerald@gmail.com>
2023-10-12 06:54:02 -0700

    added function prototype > main.h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,15 @@
  */
 int main(void)
 {
-	char message[] = "_putchar\n";
-	write(STDOUT_FILENO, message, sizeof(message) - 1);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+        _putchar('c');
+        _putchar('h');
+	_putchar('a');
+        _putchar('r');
+        _putchar('\n');
 
 	return (0);
 }
