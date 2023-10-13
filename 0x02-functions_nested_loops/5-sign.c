f527d06e03970437d8faa46e2abebc126a386781
EmeraldZhu <zhuemerald@gmail.com>
2023-10-13 02:33:58 -0700

    5. Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * printf_sign - prints the sign of a number
+ *
+ * @n - the number to be checked
+ * Return: Returns 1 if positive, 0 if 0, -1 if negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		printf("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		printf(0);
+		return (0);
+	}
+	else if (n < 0)
+	{
+		printf("-");
+		return (-1);
+	}
+}
