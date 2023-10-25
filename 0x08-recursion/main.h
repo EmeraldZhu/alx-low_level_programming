4e37f05013cc20309857bdbc7b9ab9b675ff911e
EmeraldZhu <zhuemerald@gmail.com>
2023-10-25 14:17:01 -0700

    8. Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -12,5 +12,6 @@ int my_sqrt(int my_num, int my_root);
 int is_prime_number(int n);
 int divisible(int my_num, int divisor);
 int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 
 #endif /* MAIN_H */
