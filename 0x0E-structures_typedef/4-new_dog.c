c254cfb1811a7959841fec26d5f3279d5b1d7f69
EmeraldZhu <zhuemerald@gmail.com>
2023-11-07 08:11:46 -0800

    hot fix - 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,35 +1,29 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
-
 /**
  * new_dog - creates new dog
  * @name: name of the dog
  * guok: means dog in LUO
  * @age: age of the dog
  * @owner: owner of the dog
- *
  * Return: a dog objec when correct
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	int i, len1, len2;
-
 	dog_t *guok;
 	char *a, *b;
 
 	len1 = 0;
 	for (i = 0; name[i] != '\0'; i++)
 		len1++;
-
 	len2 = 0;
 	for (i = 0; owner[i] != '\0'; i++)
 		len2++;
-
 	guok = malloc(sizeof(dog_t));
 	if (guok == NULL)
 		return (NULL);
-
 	a = malloc((len1 + 1) * sizeof(char));
 	if (a == NULL)
 	{
@@ -52,6 +46,5 @@ dog_t *new_dog(char *name, float age, char *owner)
 		b[i] = owner[i];
 	b[i] = '\0';
 	guok->owner = b;
-
 	return (guok);
 }
