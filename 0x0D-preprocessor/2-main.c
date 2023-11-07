7bc533409b14ac6b8bbfc49f003ce18eacf2109a
EmeraldZhu <zhuemerald@gmail.com>
2023-11-07 07:14:34 -0800

    2. File name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints itself
+ * Description: prints the name of the source file
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
